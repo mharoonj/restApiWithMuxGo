1bcbb413f75dfc805aed704055a5469deb99fbef
mharoonj <mharoon.j@hotmail.com>
2021-10-28 16:26:18 +0500

    initial step done

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mharoonj/restApiWithMuxGo
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,73 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+//book struct (Model)   it works similar to class
+
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Init books var as a slice Book struct
+var books []Book
+
+// get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// delete a book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
 func main() {
-	fmt.Println("Hello, 世界")
+	// init router
+	r := mux.NewRouter()
+
+	// Mock Data
+	books = append(books, Book{ID: "1", Isbn: "456123", Title: "Book one", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+
+	books = append(books, Book{ID: "2", Isbn: "7418545", Title: "Book two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
+
+	// Route Handlers /Endponts
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBooks).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r)) //if it fails we are going to get error logged
 }
